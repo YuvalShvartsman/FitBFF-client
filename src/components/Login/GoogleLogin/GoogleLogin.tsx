a27a513800eeead805185273755341a911caa727
benada1 <benadziashvili@gmail.com>
2024-07-17 18:42:14 +0300

    created a file called axios instance which saves the base url of the server and allows me to access the server by using that instance and not needing to use axios in every file that needs to send data to the server

--- src/components/Login/GoogleLogin/GoogleLogin.tsx
+++ src/components/Login/GoogleLogin/GoogleLogin.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import UseGoogleLogin from './UseGoogleLogin/UseGoogleLogin';
+import React from "react";
+import UseGoogleLogin from "./UseGoogleLogin/UseGoogleLogin";
+import instance from "../../../axiosInstance";
 
-const clientId="20513430831-2s88uppgtrbfomn25p7ooui5qfmluv7k.apps.googleusercontent.com"
+const clientId =
+  "20513430831-2s88uppgtrbfomn25p7ooui5qfmluv7k.apps.googleusercontent.com";
 
 const GoogleLogin: React.FC = () => {
   const onLoginSuccess = async (token: string) => {
     try {
-      const response = await axios.post('/api/google-login', { token });
-      console.log('Server response:', response.data);
-    } 
-    catch (error) {
-      console.error('Error sending token to server:', error);
+      const response = await instance.post("/users/google-signIn", { token });
+      console.log("Server response:", response.data);
+    } catch (error) {
+      console.error("Error sending token to server:", error);
     }
   };
 
   const onLoginFailure = (error: any) => {
-    console.error('Login failed:', error);
+    console.error("Login failed:", error);
   };
 
-  const { handleLogin } = UseGoogleLogin(clientId, onLoginSuccess, onLoginFailure);
-
-  return (
-    <button onClick={handleLogin}>
-      Login with Google
-    </button>
+  const { handleLogin } = UseGoogleLogin(
+    clientId,
+    onLoginSuccess,
+    onLoginFailure
   );
+
+  return <button onClick={handleLogin}>Login with Google</button>;
 };
 
 export default GoogleLogin;
