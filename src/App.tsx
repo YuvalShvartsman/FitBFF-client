0c35d8b2f12c7f9cf7e0b6dd491188a8bb2dd10d
oded <odedbabayov@gmail.com>
2024-11-26 20:28:24 +0200

    initial push

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import UserPreferences from "./components/UserPreferences/UserPreferences";
 import Login from "./components/Login/Login";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { useNavigate } from "react-router-dom";
+import HomePage from "./components/Homepage/Homepage";
 
 function App() {
   return (
@@ -13,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/user-preferences" element={<UserPreferences />} />
+        <Route path="/HomePage" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
