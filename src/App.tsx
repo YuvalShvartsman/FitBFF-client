a231e587c13284d378672a85a9476a44ea2201f8
oded <odedbabayov@gmail.com>
2024-12-09 20:16:53 +0200

    navigate

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import UserPreferences from "./components/UserPreferences/UserPreferences";
 import Login from "./components/Login/Login";
 import HomePage from "./components/Homepage/Homepage";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/user-preferences" element={<UserPreferences />} />
         <Route path="/HomePage" element={<HomePage />} />
+        <Route path="*" element={<Navigate to={"HomePage"} />} />
       </Routes>
     </BrowserRouter>
   );
