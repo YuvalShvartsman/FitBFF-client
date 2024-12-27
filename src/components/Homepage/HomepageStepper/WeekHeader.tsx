b89f8b6a1f45c0920b5b3ff3e4d23f6c7ea00dc0
Yuval <yuval.shvartsman@gmail.com>
2024-12-27 18:11:52 +0200

    fixed responsiveness of WeekHeader component

--- src/components/Homepage/HomepageStepper/WeekHeader.tsx
+++ src/components/Homepage/HomepageStepper/WeekHeader.tsx
@@ -8,16 +8,14 @@ type WeekHeaderProps = {
 
 function WeekHeader({ week }: WeekHeaderProps) {
   return (
-    <div className="relative h-[100px] w-[800px] text-light bg-blue-200 shadow-lg rounded-xl first:mt-16 ">
-      <span className="absolute top-5 left-5 font-bold">
-        {"week - " + week.weekNum}
-      </span>
-      <span className="absolute bottom-5 left-5 font-extrabold text-xl">
-        {"focus - " + week.title}
-      </span>
-      <div className="absolute right-5 top-5">
-        <FaInfoCircle className="cursor-pointer text-3xl " />
+    <div className="flex  h-[100px] w-full text-light bg-blue-200 shadow-lg rounded-xl first:mt-16 justify-between items-center p-10">
+      <div className="flex flex-col justify-center items-center">
+        <span className=" font-extrabold text-xl">
+          {"focus - " + week.title}
+        </span>
+        <span className=" font-bold">{"week - " + week.weekNum}</span>
       </div>
+      <FaInfoCircle className=" cursor-pointer text-3xl " />
     </div>
   );
 }
